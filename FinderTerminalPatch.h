b525128fb7650690eceeff11861ca9a14e1a5de9
Ivan Kuchin <ivan@workisfun.ru>
2011-01-25 15:54:15 +0300

    fixed project script, empty FinderTerminalPatch

---
+++ FinderTerminalPatch.h
@@ -0,0 +1,13 @@
+//
+//  FinderTerminalPatch.h
+//  FinderPatch
+//
+//  Created by toy on 25.01.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FinderPatch.h"
+
+@interface FinderTerminalPatch : NSObject
+
+@end
---
+++ FinderTerminalPatch.m
@@ -0,0 +1,17 @@
+//
+//  FinderTerminalPatch.m
+//  FinderPatch
+//
+//  Created by toy on 25.01.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FinderTerminalPatch.h"
+
+@implementation FinderTerminalPatch
+
++ (void) load {
+	NSLog(@"%@ loaded", [self class]);
+}
+
+@end
